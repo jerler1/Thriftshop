bb8e8c4910bf249d2b0fac30b8d4c354511da268
Bunks <busk_1@yahoo.com>
2021-02-23 16:09:36 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AddItem from "./pages/Admin/AddItem/AddItem";
+import Dashboard from "./pages/Admin/Dashboard/Dashboard";
+import EditItem from "./pages/Admin/EditItem/EditItem";
+import Login from "./pages/Admin/Login/Login";
+import Detail from "./pages/Detail/Detail";
+import Home from "./pages/Home/Home";
+import Listing from "./pages/Listings/Listing";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
+  <Router>
+    return (
+    <div>
+      {/* Navbar here */}
+      <main className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/listing" component={Listing} />
+          <Route exact path="/listing/:id" component={Detail} />
+          <Route exact path="/admin" component={Login} />
+          <Route exact path="/admin/dashboard" component={Dashboard} />
+          <Route exact path="/admin/addItem" component={AddItem} />
+          <Route exact path="/admin/editItem/:id" component={EditItem} />
+        </Switch>
+        </main>;
     </div>
-  );
+    )
+  </Router>;
 }
 
 export default App;
