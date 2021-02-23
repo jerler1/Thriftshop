a46c249f82a22350af1b71c5cbfd276c3fa28cea
Nathan Castaldi <nathan@castaldi.dev>
2021-02-23 16:55:09 -0500

    renamed file

--- models/invoice.js
+++ models/invoice.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
+const invoiceSchema = new Schema({
     storeID: {
         type: mongoose.Schema.Types.ObjectId,
         ref: Storefront
@@ -14,6 +14,6 @@ const orderSchema = new Schema({
     //TODO: Add array of items to be purchased.
 });
 
-const Order = mongoose.model("Order", orderSchema);
+const Invoice = mongoose.model("Invoice", invoiceSchema);
 
-module.exports = Order;
+module.exports = Invoice;
