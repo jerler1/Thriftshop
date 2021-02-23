9fe899b7c1e242c0860914a38fb8244003c6d0b7
Nathan Castaldi <nathan@castaldi.dev>
2021-02-23 16:26:21 -0500

    created employee model

---
+++ models/employee.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  storefront: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: storefront,
+  },
+  isAdmin: {
+    type: Boolean,
+    trim: true,
+  },
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
