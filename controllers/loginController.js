189453d9bb5ad2de7b5bdbe1cebd3e5e61f95d6f
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-24 00:15:14 -0500

    Adjust for testing

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,12 @@ const login = (req, res) => {
   const { email, password } = req.body;
   // do stuff with db;
   console.log(`Received email: '${email}' and password: '${password}'.`);
-  res.json({ status: "ok" });
+  const user = {
+    username: "Test User",
+  };
+  res.json({
+    user,
+  });
 };
 
 module.exports = login;
