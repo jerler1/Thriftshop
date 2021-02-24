d71e00f5277836065e07a9eb4eb596458372d03d
Bunks <busk_1@yahoo.com>
2021-02-24 11:48:48 -0500

    added section and columns

--- client/src/pages/Admin/AddItem/AddItem.jsx
+++ client/src/pages/Admin/AddItem/AddItem.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import "./AddItem.css"
+import React from "react";
+import "./AddItem.css";
 
 const AddItem = (props) => {
-    return (
-        <div>
-            <h1>Add items here.</h1>
+  return (
+    <div>
+      <section className="section">
+          <h1>This is the add item.</h1>
+      </section>
+      <div className="columns">
+        <div className="column has-background-warning-dark">
+            {/* Upload to cloudinary */}
         </div>
-    );
+        <div className="column has-background-danger-dark">
+            {/* Form */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddItem;
-
