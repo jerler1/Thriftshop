1db8b546d785cbb3641a5af4c8b9951319229759
Nathan Castaldi <nathan@castaldi.dev>
2021-02-24 12:17:35 -0500

    added routes for all and by id

---
+++ routes/inventory.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const inventoryController = require("../controllers/inventoryController");
+
+// Matches with "/api/inventory"
+router
+    .route("/")
+    .get(inventoryController.findAll)
+    .post(inventoryController.create);
+
+// Matches with "/api/inventory/:id"
+router
+    .route("/:id"
+        .get(inventoryController.findById)
+        .put(inventoryController.update)
+        .delete(inventoryController.remove);
+
+module.exports = router;
