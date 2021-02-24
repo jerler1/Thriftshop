7b6e866a89536fa46696a582c02d8f9b515ba947
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-24 14:47:24 -0500

    split the separate footers into admin and client components

---
+++ client/src/components/AdminFooter/AdminFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminFooter = () => {
+    return (
+        <div className="card-footer admin-footer">
+            <a className="card-footer-item">Unlist</a>
+            <a className="card-footer-item">Edit</a>
+            <a className="card-footer-item">Remove</a>
+        </div>
+    );
+};
+
+export default AdminFooter;
---
+++ client/src/components/ClientFooter/ClientFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClientFooter = () => {
+    return (
+        <div className="card-footer admin-footer">
+            <a className="card-footer-item">Reserve</a>
+            <a className="card-footer-item">Add to Cart</a>
+        </div>
+    );
+};
+
+export default ClientFooter;
