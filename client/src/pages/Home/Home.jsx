bae07b968b102ed22a2ee76ac6204a43de93c519
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-24 14:22:04 -0500

    imported Gallery container to the home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Home.css";
+import GalleryContainer from "../../components/GalleryContainer/GalleryContainer";
 
 const Home = (props) => {
     return (
         <div>
             <h1>Home</h1>
+            <GalleryContainer />
         </div>
     );
 };
