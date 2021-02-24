5f67af090360dc2cff8c184969fbbe91263b7b26
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-24 13:52:28 -0500

    Update login/logout routes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const api = {
   login(email, password) {
     return axios
-      .post("/login", { email, password })
+      .post("/api/login", { email, password })
       .then(({ data }) => data)
       .catch((err) => console.log("API error: ", err));
   },
   logout() {
-    return axios.get("/logout")
+    return axios.get("/api/logout")
       .then(({ data }) => data)
       .catch((err) => console.log("API ERROR: ", err));
   }
