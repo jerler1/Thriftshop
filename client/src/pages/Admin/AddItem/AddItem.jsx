536527d7c99e6bad96822b531693f2c9f74c5755
Bunks <busk_1@yahoo.com>
2021-02-24 18:25:27 -0500

    transfered form from additem to itemform

--- client/src/pages/Admin/AddItem/AddItem.jsx
+++ client/src/pages/Admin/AddItem/AddItem.jsx
@@ -35,53 +35,7 @@ const AddItem = (props) => {
           </button>
         </div>
         <div className="column has-background-danger-dark">
-          <div class="field">
-            <label class="label">Label</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="e.g. Dresser" />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Category</label>
-            <div class="control">
-              <div className="select">
-                <select name="category">
-                  <option value="Select One">Select One</option>
-                  <option value="Furniture">Furniture</option>
-                  <option value="Blender">Blender</option>
-                  <option value="Sofa">Sofa</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Price</label>
-            <div class="control">
-              <input class="input" type="text" placeholder="e.g. $10" />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Condition</label>
-            <div class="control">
-              <div className="select">
-                <select name="category">
-                  <option value="Select One">Select One</option>
-                  <option value="Slightly Damaged">Slightly Damaged</option>
-                  <option value="Destroyed">Destroyed</option>
-                  <option value="Moderately Dinged">Moderately Dinged</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Description</label>
-            <div class="control">
-              <textarea
-                class="textarea is-primary"
-                placeholder="Primary textarea"
-              ></textarea>
-            </div>
-          </div>
+
         </div>
       </div>
     </div>
