e37fd94a7f4253e5e5c45e4672d94b859d0f145e
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-24 14:48:17 -0500

    adjusted the itemcard component to remove redundant info on the gallery container

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,34 +1,31 @@
 import React from "react";
+import AdminFooter from "../AdminFooter/AdminFooter";
 import "./ItemCard.css";
 
-const ItemCard = () => {
+const ItemCard = (props) => {
   return (
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src="https://www.placecage.com/1280/960"
-            alt="Placeholder image"
-          />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div>
-            <p className="title is-4">Thrift Item</p>
-            <p className="subtitle is-7">Store Section</p>
+    <div className="column">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src="https://www.placecage.com/1280/960"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div>
+              <p className="title is-4">Thrift Item</p>
+              <p className="subtitle is-7">Store Section</p>
+            </div>
           </div>
-        </div>
 
-        <div className="content">
-          A short amount of details about the item listed here. 
-        </div>
-        <div className="card-footer admin-footer">
-            {/* <a className="card-footer-item">Unlist</a>
-            <a className="card-footer-item">Edit</a>
-            <a className="card-footer-item">Remove</a> */}
-            <a className="card-footer-item">Reserve</a>
-            <a className="card-footer-item">Add to Cart</a>
+          <div className="content">
+            A short amount of details about the item listed here.
+          </div>
+          {props.children}
         </div>
       </div>
     </div>
