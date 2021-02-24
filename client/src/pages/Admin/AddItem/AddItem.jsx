292b29d04247e71e4b26424e0c5600dc60eb6c5f
Bunks <busk_1@yahoo.com>
2021-02-24 13:51:35 -0500

    implemented cloudinary and allows uploads

--- client/src/pages/Admin/AddItem/AddItem.jsx
+++ client/src/pages/Admin/AddItem/AddItem.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import "./AddItem.css";
-import {Image} from "cloudinary-react";
+import { Image } from "cloudinary-react";
 
 const AddItem = (props) => {
-    const uploadImage = async 
 
-
+  const widget = window.cloudinary.createUploadWidget(
+    {
+      cloudName: "thriftshopshop",
+      uploadPreset: "thriftshopshop",
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info);
+      }
+    }
+  );
 
   return (
     <div>
       <section className="section">
-          <h1>This is the add item.</h1>
+        <h1>This is the add item.</h1>
       </section>
       <div className="columns">
         <div className="column has-background-warning-dark">
-            {/* Upload to cloudinary */}
-            <button onClick={uploadImage}>Upload an Image</button>
+          {/* Upload to cloudinary */}
+          <button onClick={widget.open}>Upload an Image</button>
         </div>
-        <div className="column has-background-danger-dark">
-            {/* Form */}
-        </div>
+        <div className="column has-background-danger-dark">{/* Form */}</div>
       </div>
     </div>
   );
