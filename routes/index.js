94a142b80369097d91bc934a26c6feb9d60c94e1
Nathan Castaldi <nathan@castaldi.dev>
2021-02-24 12:10:19 -0500

    added inclusion of inventory routes file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const loginRoutes = require("./login");
+const inventoryRoutes = require("./inventory");
 
 router.use(loginRoutes);
 
