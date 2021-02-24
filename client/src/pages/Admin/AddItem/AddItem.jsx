bd76af89ef3b27f2e27ec63f659b1072efa3fcb8
Bunks <busk_1@yahoo.com>
2021-02-24 17:53:49 -0500

    added a form

--- client/src/pages/Admin/AddItem/AddItem.jsx
+++ client/src/pages/Admin/AddItem/AddItem.jsx
@@ -15,7 +15,7 @@ const AddItem = (props) => {
     (error, result) => {
       if (!error && result && result.event === "success") {
         console.log("Done! Here is the image info: ", result.info);
-        setImageSource(result.info.url)
+        setImageSource(result.info.url);
       }
     }
   );
@@ -25,14 +25,79 @@ const AddItem = (props) => {
       <section className="section">
         <h1>This is the add item.</h1>
       </section>
-      <div className="columns">
-        <div className="column has-background-warning-dark">
-          <figure className="image is-1by1">
-            <img src={imageSource} alt="placeholder"/>
+      <div className="columns center">
+        <div className="column has-background-warning-dark leftCol">
+          <figure className="image center">
+            <img src={imageSource} alt="placeholder" />
           </figure>
-          <button onClick={widget.open}>Upload Image</button>
+          <button className="center" onClick={widget.open}>
+            Upload Image
+          </button>
         </div>
-        <div className="column has-background-danger-dark">{/* Form */}</div>
+        <div className="column has-background-danger-dark">
+          <div class="field" />
+          <label class="label">Name</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Username</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-success"
+              type="text"
+              placeholder="Text input"
+              value="bulma"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+          <p class="help is-success">This username is available</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-danger"
+              type="email"
+              placeholder="Email input"
+              value="hello@"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+          <p class="help is-danger">This email is invalid</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Subject</label>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Message</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+          </div>
+        </div>
       </div>
     </div>
   );
