3891cf1dc29f3fd3b5a41390a96e2380ca6f8eb4
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-25 15:07:41 -0500

    Remove catch from api calls
    Not sure if this is the best way of handling it, but it seems better
    to just let the failed promise be handled by the caller rather
    than catching it here and rethrowing.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,30 +2,19 @@ import axios from "axios";
 
 const api = {
   login(email, password) {
-    return axios
-      .post("/api/login", { email, password })
-      .then(({ data }) => data)
-      .catch((err) => console.log("API error: ", err));
+    return axios.post("/api/login", { email, password }).then(({ data }) => data);
   },
   logout() {
-    return axios
-      .get("/api/logout")
-      .then(({ data }) => data)
-      .catch((err) => console.log("API ERROR: ", err));
+    return axios.get("/api/logout").then(({ data }) => data);
   },
   addItemSubmit(formObject) {
-    return axios
-      .post("/api/inventory", formObject)
-      .then(({ data }) => {
-        console.log(data);
-      })
-      .catch((err) => console.log("API error: ", err));
+    return axios.post("/api/inventory", formObject).then(({ data }) => data);
   },
-  getStorefront(id) {
-    return axios
-      .get(`/api/storefront/${id}`)
-      .then(({ data }) => data)
-      .catch((err) => err);
+  getStorefront(storeId) {
+    return axios.get(`/api/storefront/${storeId}`).then(({ data }) => data);
+  },
+  deleteItem(id) {
+    return axios.delete(`/api/inventory/${id}`).then(({ data }) => data);
   },
 };
 
