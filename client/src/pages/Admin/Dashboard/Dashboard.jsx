9487b7e21d8cef5e3511a3f7b9fc4ceb4bea13cf
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-25 11:27:33 -0500

    Basic dashboard

--- client/src/pages/Admin/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -1,31 +1,85 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import { useAuth } from "../../../hooks/use-auth";
-import GalleryContainer from "../../../components/GalleryContainer/GalleryContainer";
-import api from "../../../api/index";
 import "./Dashboard.css";
 
 const Dashboard = (props) => {
   const auth = useAuth();
 
-  // TODO: Use storefrontId from user object to make request.
-  // Or should we populate the user object on login and then we don't need to make the request here?
-  useEffect(() => {
-    api
-      .getStorefront("6036cfaaa025eec02d37be13")
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // TODO: Use the actual items from the store on the user object.
+  const items = [
+    {
+      id: 1,
+      name: "Oak Dresser",
+      description: "fancy dresser",
+      category: "Furniture",
+      price: "150",
+      condition: "Good",
+      image: "",
+      status: "Sold",
+    },
+    {
+      id: 2,
+      name: "Oak Dresser",
+      description: "fancy dresser",
+      category: "Furniture",
+      price: "150",
+      condition: "Good",
+      image: "",
+      status: "Sold",
+    },
+    {
+      id: 3,
+      name: "Oak Dresser",
+      description: "fancy dresser",
+      category: "Furniture",
+      price: "150",
+      condition: "Good",
+      image: "",
+      status: "Sold",
+    },
+  ];
+
+  const tableHeadings = Object.keys(items[0]).filter((heading) => heading !== "id");
 
   return auth.user ? (
     <div className="container">
-      <h2 className="title">Welcome back, {auth.user.username}</h2>
-      <h3 className="subtitle">You currently have 8 items in your store.</h3>
-      <GalleryContainer />
+      <header className="mb-4">
+        <h2 className="title">Welcome back, {auth.user.username}</h2>
+      </header>
+      <section className="mb-4">
+        <h2 className="title">Metrics?</h2>
+      </section>
+      <table className="table is-striped is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            {tableHeadings.map((heading, i) => (
+              <th key={i}>{heading}</th>
+            ))}
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.id}>
+              {tableHeadings.map((heading, i) => (
+                <td key={i}>{item[heading]}</td>
+              ))}
+              <td className="is-flex is-justify-content-space-around">
+                <Link to={`/admin/editItem/${item.id}`}>
+                  <span className="icon">
+                    <i className="far fa-edit"></i>
+                  </span>
+                </Link>
+                <span className="icon has-text-danger is-clickable">
+                  <i className="fas fa-trash-alt"></i>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   ) : (
     <Redirect to="/admin" />
