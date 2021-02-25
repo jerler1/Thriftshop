6499df4ba4e87ad259224b09ff6d669fa0b25da6
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-25 13:57:37 -0500

    added contrast to hero image for readability

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 
 const Hero = () => {
     return (
-        <section class="hero is-medium hero-img">
+        <section class="hero is-medium hero-img mb-6">
         <div class="hero-body hero-block">
           <p class="title hero-title">Thrift Shop²</p>
           <p class="subtitle hero-subtitle">Browse at your leisure!</p>
