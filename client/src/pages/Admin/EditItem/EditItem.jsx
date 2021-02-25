9fa176e47dbd9dd41bea6e62c3cbc356cc66c96b
Bunks <busk_1@yahoo.com>
2021-02-25 11:07:22 -0500

    initial values copied from addItem

--- client/src/pages/Admin/EditItem/EditItem.jsx
+++ client/src/pages/Admin/EditItem/EditItem.jsx
@@ -1,12 +1,71 @@
-import React from 'react';
-import "./EditItem.css";;
+import React, { useState } from "react";
+import ItemForm from "../../../components/ItemForm/ItemForm";
+import "./EditItem.css";
+import API from "../../../api/index";
 
-const EditItem = (props) => {
-    return (
-        <div>
-            <h1>Edit items.</h1>
+const EditItem = () => {
+  const [imageSource, setImageSource] = useState(
+    "http://res.cloudinary.com/thriftshopshop/image/upload/v1614196967/thriftshopshop/hzkbtocbzgzenprljfao.jpg"
+  );
+  const [formValues, setFormValues] = useState({
+    name: "",
+    category: "",
+    price: "",
+    condition: "",
+    description: "",
+  });
+
+  const widget = window.cloudinary.createUploadWidget(
+    {
+      cloudName: "thriftshopshop",
+      uploadPreset: "thriftshopshop",
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info);
+        setImageSource(result.info.url);
+      }
+    }
+  );
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    API.addItemSubmit(formValues)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <div className="columns center columnsCustom">
+        <div className="column leftCol">
+          <figure className="imageCustom center">
+            <img src={imageSource} alt="placeholder" />
+          </figure>
+          <button className="button is-info center" onClick={widget.open}>
+            Upload Image
+          </button>
         </div>
-    );
+        <div className="column rightCol">
+          <ItemForm
+            {...formValues}
+            handleInputChange={handleInputChange}
+            handleFormSubmit={handleFormSubmit}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditItem;
