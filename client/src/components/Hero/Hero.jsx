4836a197b3b44055503bcf7496640938c394282e
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-25 11:40:35 -0500

    built a basic hero for the landing page

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import heroImg from "../../images/thrift-image1.png";
+
+const Hero = () => {
+    return (
+        <section class="hero is-medium hero-img">
+        <div class="hero-body hero-block">
+          <p class="title hero-title">Thrift Shop²</p>
+          <p class="subtitle hero-subtitle">Browse at your leisure!</p>
+        </div>
+      </section>
+    );
+};
+
+export default Hero;
