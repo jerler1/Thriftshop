262bb713f3608f1763b2635e7aa7a31f717bfffa
Nathan Castaldi <nathan@castaldi.dev>
2021-02-25 11:06:09 -0500

    refactored to match formatting of other controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,20 @@
-const login = (req, res) => {
-  const { email, password } = req.body;
-  // do stuff with db;
-  console.log(`Received email: '${email}' and password: '${password}'.`);
-  const user = {
-    username: "Test User",
-  };
-  res.json({
-    user,
-  });
-};
-
-const logout = (req, res) => {
-  // destroy the session 
-  res.status(200).send();
-}
+const db = require("../models");
 
 module.exports = {
-  login,
-  logout,
-};
+  login: function (req, res) {
+    const { email, password } = req.body;
+    // do stuff with db;
+    console.log(`Received email: '${email}' and password: '${password}'.`);
+    const user = {
+      username: "Test User",
+    };
+    res.json({
+      user,
+    });
+  },
+  logout: function (req, res) {
+    // destroy the session 
+    res.status(200).send();
+  }
+
+}
