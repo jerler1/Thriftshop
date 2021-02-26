a68d09efda8fb7fbb01c1c91a57dd1f337f9aa96
Nathan Castaldi <nathan@castaldi.dev>
2021-02-26 11:48:03 -0500

    created basic createEmployee method with bcrypt hash

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,16 @@
 const db = require("../models");
+const bcrypt = require("bcrypt");
 
 module.exports = {
+  createEmployee: function(req, res) {
+    const employeeToCreate = {
+      email: req.body.email,
+    };
+    bcrypt.hash(req.body.password, 8, (err, hashedPassword) => {
+      if (err) throw new Error(err);
+      console.log(hashedPassword);
+    });
+  },
   login: function (req, res) {
     const { email, password } = req.body;
     db.Employee.findOne({ email: email })
@@ -11,5 +21,4 @@ module.exports = {
     // destroy the session 
     res.status(200).send();
   }
-
 }
