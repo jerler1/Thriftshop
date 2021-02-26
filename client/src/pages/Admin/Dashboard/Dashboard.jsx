d17cb8e71c8a196b25ce78b49c0115b861388e19
Bunks <busk_1@yahoo.com>
2021-02-26 14:26:32 -0500

    added the dashboard table to a component and refactored

--- client/src/pages/Admin/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -2,28 +2,29 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../hooks/use-auth";
 import api from "../../../api";
+import Chart from "../../../components/DashboardChart/DashboardChart";
 import "./Dashboard.css";
 
 const Dashboard = (props) => {
   const auth = useAuth();
-  const [storeItems, setStoreItems] = useState([{
-    name: "",
-    description: "",
-    category: "",
-    price: "",
-    condition: "",
-    image: [],
-    status: "",
-    storefront: "",
-  }]);
+  const [storeItems, setStoreItems] = useState([
+    {
+      name: "",
+      description: "",
+      category: "",
+      price: "",
+      condition: "",
+      image: [],
+      status: "",
+      storefront: "",
+    },
+  ]);
 
   useEffect(() => {
     api
       .getStorefront(auth.user.storefront)
       .then((store) => {
-        console.log(store);
         setStoreItems(store.items);
-        console.log(store.items);
       })
       .catch((err) => {
         // Do something with error.
@@ -49,10 +50,6 @@ const Dashboard = (props) => {
       });
   };
 
-  const tableHeadings = Object.keys(storeItems[0]).filter(
-    (heading) => heading !== "id"
-  );
-
   return (
     <div className="container">
       <header className="mb-4">
@@ -61,46 +58,7 @@ const Dashboard = (props) => {
       <section className="mb-4">
         <h2 className="title">Metrics?</h2>
       </section>
-      <table className="table is-striped is-hoverable is-fullwidth">
-        <thead>
-          <tr>
-            {tableHeadings.map((heading, i) => (
-              <th key={i}>{heading}</th>
-            ))}
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {storeItems.map((item) => (
-            <tr key={item.id}>
-              {tableHeadings.map((heading, i) => {
-                if (heading === "image") {
-                  return (
-                    <td>
-                      <img key={i} src={item.image[0]} />
-                    </td>
-                  );
-                } else {
-                  return <td key={i}>{item[heading]}</td>;
-                }
-              })}
-              <td className="is-flex is-justify-content-space-around">
-                <Link to={`/admin/editItem/${item._id}`}>
-                  <span className="icon">
-                    <i className="far fa-edit"></i>
-                  </span>
-                </Link>
-                <span
-                  className="icon has-text-danger is-clickable"
-                  onClick={() => handleDelete(item._id)}
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Chart storeItems={storeItems} handleDelete={handleDelete} />
     </div>
   );
 };
