a031faf9b8a725b55897052b0900d0f443dd7c53
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-25 19:50:52 -0500

    imported the api and built the items state, to load on inventory with useEffect and map out to each card

--- client/src/components/GalleryContainer/GalleryContainer.jsx
+++ client/src/components/GalleryContainer/GalleryContainer.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ItemCard from "../ItemCard/ItemCard";
 import AdminFooter from "../AdminFooter/AdminFooter";
 import ClientFooter from "../ClientFooter/ClientFooter";
 import { useAuth } from "../../hooks/use-auth";
+import api from "../../api/index";
 import "../ItemCard/ItemCard.css";
 
 const GalleryContainer = () => {
+  const [items, setItems] = useState([]);
   const auth = useAuth();
+
+  useEffect(() => {
+    loadInventory();
+  }, []);
+
+
+  function loadInventory() {
+    api
+      .getInventory()
+      .then((res) => setItems(res.data))
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div className="container">
-      <div className="columns">
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-      </div>
-      <div className="columns">
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
-        <ItemCard>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>
+      <div className="columns is-multiline">
+        {items.map(item => {
+          return (<ItemCard key={item._id} item={item}>{auth.user ? <AdminFooter /> : <ClientFooter />}</ItemCard>);
+        })}
       </div>
     </div>
   );
