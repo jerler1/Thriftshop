63fba8429d28c956862e8b95ddde583aef315e78
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-25 23:59:12 -0500

    Update models and seeds

--- models/invoice.js
+++ models/invoice.js
@@ -5,38 +5,32 @@ const Storefront = require("./storefront");
 const Schema = mongoose.Schema;
 
 const invoiceSchema = new Schema({
-    storeID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: Storefront,
-        required: true,
+  storeID: {
+    type: Schema.Types.ObjectId,
+    ref: Storefront,
+    required: true,
+  },
+  customerName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  customerEmail: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  purchasedItems: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: Inventory,
     },
-    customerName: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    customerEmail: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    purchasedItems: [
-        {
-            itemID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: Inventory,
-            },
-            description: {
-                type: String,
-                ref: Inventory,
-            }
-        }
-    ],
-    status: {
-        type: String,
-        trim: true,
-        required: true,
-    }
+  ],
+  status: {
+    type: String,
+    trim: true,
+    required: true,
+  },
 });
 
 const Invoice = mongoose.model("Invoice", invoiceSchema);
