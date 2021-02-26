379a0f387b1b60ea3d225e9af50546eb052e2ca8
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-26 11:00:45 -0500

    fixed error in getallitems call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,7 +10,7 @@ const api = {
     return axios.get("/api/logout").then(({ data }) => data);
   },
   getInventory() {
-    return axios.get("api/inventory");
+    return axios.get("/api/inventory");
   },
   addItemSubmit(formObject) {
     return axios.post("/api/inventory", formObject).then(({ data }) => data);
