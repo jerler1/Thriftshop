4d3a41944446d168c9d576041041e543873cf218
Nathan Castaldi <nathan@castaldi.dev>
2021-02-26 11:58:35 -0500

    updated createEmployee method to capture inputed data and send to db; tested working locally

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,11 +4,20 @@ const bcrypt = require("bcrypt");
 module.exports = {
   createEmployee: function(req, res) {
     const employeeToCreate = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
       email: req.body.email,
+      storefront: req.body.storefront,
+      isAdmin: req.body.isAdmin,
     };
     bcrypt.hash(req.body.password, 8, (err, hashedPassword) => {
       if (err) throw new Error(err);
       console.log(hashedPassword);
+      employeeToCreate.password = hashedPassword;
+      db.Employee.create(employeeToCreate).then((newEmployee) => {
+        //FIXME: Don't send back the user
+        res.json(newEmployee);
+      })
     });
   },
   login: function (req, res) {
