bfb80a6777dfedc55bfe17d4e1bfc13a113ecff0
Bunks <busk_1@yahoo.com>
2021-02-26 14:46:29 -0500

    removed the link import from dashboard

--- client/src/pages/Admin/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { useAuth } from "../../../hooks/use-auth";
 import api from "../../../api";
 import Chart from "../../../components/DashboardChart/DashboardChart";
