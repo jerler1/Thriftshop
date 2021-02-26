a7a91d149a0480ded623b89d19e0dd8296dd5a4e
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-25 19:52:33 -0500

    added details button to the client footer

--- client/src/components/ClientFooter/ClientFooter.jsx
+++ client/src/components/ClientFooter/ClientFooter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const ClientFooter = () => {
     return (
         <div className="card-footer admin-footer">
-            <a href="/" className="card-footer-item">Reserve</a>
-            <a href="/" className="card-footer-item">Add to Cart</a>
+            <Link to="/listing/:id" className="card-footer-item">See Details</Link>
+            <Link to="/" className="card-footer-item">Add to Cart</Link>
+            <Link to="/" className="card-footer-item">Reserve</Link>
         </div>
     );
 };
