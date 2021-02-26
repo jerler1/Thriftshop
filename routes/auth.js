8a6d69142c6cbfb1a523f9a1f51519867b275db0
Nathan Castaldi <nathan@castaldi.dev>
2021-02-26 11:11:46 -0500

    added 'createEmployee' route

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,7 @@ const authController = require('../controllers/authController');
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
+// Route to create new employees
+router.get("/createEmployee", authController.createEmployee);
+
 module.exports = router;
