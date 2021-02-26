d04893dfa5fc25c6759a5420e54318ed805882e1
Bunks <busk_1@yahoo.com>
2021-02-25 19:54:25 -0500

    added use effect

--- client/src/pages/Admin/AddItem/AddItem.jsx
+++ client/src/pages/Admin/AddItem/AddItem.jsx
@@ -4,10 +4,9 @@ import "./AddItem.css";
 import API from "../../../api/index";
 
 const AddItem = () => {
-
   // This state holds an URL which populates the image.
   const [imageSource, setImageSource] = useState(
-    "http://res.cloudinary.com/thriftshopshop/image/upload/v1614196967/thriftshopshop/hzkbtocbzgzenprljfao.jpg"
+    "http://res.cloudinary.com/thriftshopshop/image/https://res.cloudinary.com/thriftshopshop/image/upload/v1614298348/thriftshopshop/ccsccl977ftngs5u7caw.jpg/v1614196967/thriftshopshop/hzkbtocbzgzenprljfao.jpg"
   );
 
   // This state holds the data that is coming from the form.
@@ -19,8 +18,10 @@ const AddItem = () => {
     description: "",
   });
 
+  // Allows refering to the widget by the DOM.
   const widgetRef = useRef();
 
+  // Loading the cloudinary widget at component mount.
   useEffect(() => {
     widgetRef.current = window.cloudinary.createUploadWidget(
       {
@@ -49,7 +50,7 @@ const AddItem = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
     console.log(event);
-    API.addItemSubmit({...formValues, image: [imageSource]})
+    API.addItemSubmit({ ...formValues, image: [imageSource] })
       .then((res) => {
         console.log(res);
       })
@@ -70,7 +71,11 @@ const AddItem = () => {
           </button>
         </div>
         <div className="column rightCol">
-          <ItemForm {...formValues} handleInputChange={handleInputChange} handleFormSubmit={handleFormSubmit} />
+          <ItemForm
+            {...formValues}
+            handleInputChange={handleInputChange}
+            handleFormSubmit={handleFormSubmit}
+          />
         </div>
       </div>
     </div>
--- client/src/pages/Admin/EditItem/EditItem.jsx
+++ client/src/pages/Admin/EditItem/EditItem.jsx
@@ -5,7 +5,10 @@ import "./EditItem.css";
 import API from "../../../api/index";
 
 const EditItem = () => {
+  // Making the id from the URL available.
   const { id } = useParams();
+
+  // Making the states empty slate for when the edittable item data comes in.
   const [imageSource, setImageSource] = useState("");
   const [formValues, setFormValues] = useState({
     name: "",
@@ -15,22 +18,34 @@ const EditItem = () => {
     description: "",
   });
 
-//   useEffect(() => {
-//     API.get;
-//   }, []);
+  const widgetRef = useRef();
 
-  const widget = window.cloudinary.createUploadWidget(
-    {
-      cloudName: "thriftshopshop",
-      uploadPreset: "thriftshopshop",
-    },
-    (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log("Done! Here is the image info: ", result.info);
-        setImageSource(result.info.url);
+  useEffect(() => {
+    widgetRef.current = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "thriftshopshop",
+        uploadPreset: "thriftshopshop",
+        sources: ["local", "url", "camera"],
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          setImageSource(result.info.url);
+        }
       }
-    }
-  );
+    );
+    API.getItem(id)
+      .then((retrievedItem) => {
+        console.log(retrievedItem);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function handleClick() {
+    widgetRef.current.open();
+  }
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -56,7 +71,7 @@ const EditItem = () => {
           <figure className="imageCustom center">
             <img src={imageSource} alt="placeholder" />
           </figure>
-          <button className="button is-info center" onClick={widget.open}>
+          <button className="button is-info center" onClick={handleClick}>
             Upload Image
           </button>
         </div>
