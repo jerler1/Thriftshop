bcb18a261622a9094a52434ef572d01a892a419c
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-26 10:08:08 -0500

    adjusted name of the inventory state to be more clear

--- client/src/components/ClientFooter/ClientFooter.jsx
+++ client/src/components/ClientFooter/ClientFooter.jsx
@@ -5,9 +5,9 @@ const ClientFooter = (props) => {
     
     return (
         <div className="card-footer admin-footer" id={props.key}>
-            <Link to={`/inventory/${props.item._id}`} className="card-footer-item">See Details</Link>
-            <Link to="/" className="card-footer-item">Add to Cart</Link>
-            <Link to="/" className="card-footer-item">Reserve</Link>
+            <Link to={`/listing/${props.item._id}`} className="card-footer-item">See Details</Link>
+            <Link to="/listing" className="card-footer-item">Add to Cart</Link>
+            <Link to="/listing" className="card-footer-item">Reserve</Link>
         </div>
     );
 };
