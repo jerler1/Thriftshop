6ec84281d2d96010f01b9ac092309ad22a4eeac7
Bunks <busk_1@yahoo.com>
2021-02-28 02:42:10 -0500

    successfully hitting stripe payment form

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,30 +1,30 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-const stripe = require("stripe")(
-  "pk_test_51IPhcIG7oxYUGKJCY4GkNWBFbgXwvNKTlmmJNeLOarK1J3DSvpvI9f65OcfurdeT8zKz3vmO5eUlnP5n3AIfKR1C00tL4qrsVY"
-);
+// const stripe = require("stripe")(
+//   "sk_test_51IPhcIG7oxYUGKJCr6L1Htx1gIPshDLMp6gW1vkTl9dEmSJeVEPxqTJwU2c0xaoEklaTwFHEycrr5dUe36h4vaxg00vAFVLSjZ"
+// );
 
-router.post("/checkout", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "https://www.google.com",
-    cancel_url: "https://www.bing.com",
-  });
+// router.post("/checkout", async (req, res) => {
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items: [
+//       {
+//         price_data: {
+//           currency: "usd",
+//           product_data: {
+//             name: "T-shirt",
+//           },
+//           unit_amount: 2000,
+//         },
+//         quantity: 1,
+//       },
+//     ],
+//     mode: "payment",
+//     success_url: "https://www.google.com",
+//     cancel_url: "https://www.bing.com",
+//   });
 
-  res.json({ id: session.id });
-});
+//   res.json({ id: session.id });
+// });
 
-module.exports = router;
+// module.exports = router;
