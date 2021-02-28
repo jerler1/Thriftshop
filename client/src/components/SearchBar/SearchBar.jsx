c98f2c0527fba2f664ce864d23b89d4003d514cb
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-27 21:14:33 -0500

    Fix searchbar zindex issues

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,22 +3,14 @@ import "./SearchBar.css";
 
 const SearchBar = () => {
   return (
-    <div>
-      <div className="container">
-        <nav
-          className="navbar is-fullwidth"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div className="navbar-item search-bar">
-            <input
-              className="input is-fullwidth search-bar"
-              type="search"
-              placeholder="Search..."
-            />
+    <div className="container mb-4 p-4">
+      <form>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="search" placeholder="Search..." />
           </div>
-        </nav>
-      </div>
+        </div>
+      </form>
     </div>
   );
 };
