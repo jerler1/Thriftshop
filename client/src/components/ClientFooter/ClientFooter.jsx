e0fa7a4d138dcc0f9f647b05480b42af39befd67
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-28 18:01:06 -0500

    Update client footer buttons

--- client/src/components/ClientFooter/ClientFooter.jsx
+++ client/src/components/ClientFooter/ClientFooter.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {useCart} from "../../hooks/useCart";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 
 const ClientFooter = (props) => {
-    const cart = useCart();
-    
-    return (
-        <div className="card-footer admin-footer" id={props.key}>
-            <Link to={`/listing/${props.item._id}`} className="card-footer-item">See Details</Link>
-            <Link to="/listing" className="card-footer-item" onClick={() => {
-                cart.addToCart(props.item)
-            }}>Add to Cart</Link>
-            {/* <Link to="/listing" className="card-footer-item">Reserve</Link> */}
-        </div>
-    );
+  const cart = useCart();
+
+  return (
+    <div className="is-flex is-flex-direction-column" id={props.key}>
+      <Link to={`/listing/${props.item._id}`} className="button is-light mb-2">
+        See Details
+      </Link>
+      <button
+        className="button is-primary"
+        onClick={() => {
+          cart.addToCart(props.item);
+        }}
+      >
+        Add to Cart
+      </button>
+      {/* <Link to="/listing" className="card-footer-item">Reserve</Link> */}
+    </div>
+  );
 };
 
 export default ClientFooter;
