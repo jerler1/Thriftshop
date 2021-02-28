f8845e07b661ad822a8182d2cc13c6a8a7c831db
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-28 12:56:45 -0500

    removed the reserve button for the time being

--- client/src/components/ClientFooter/ClientFooter.jsx
+++ client/src/components/ClientFooter/ClientFooter.jsx
@@ -7,7 +7,7 @@ const ClientFooter = (props) => {
         <div className="card-footer admin-footer" id={props.key}>
             <Link to={`/listing/${props.item._id}`} className="card-footer-item">See Details</Link>
             <Link to="/listing" className="card-footer-item">Add to Cart</Link>
-            <Link to="/listing" className="card-footer-item">Reserve</Link>
+            {/* <Link to="/listing" className="card-footer-item">Reserve</Link> */}
         </div>
     );
 };
