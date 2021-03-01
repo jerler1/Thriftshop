f0614b61018d35960773eb5c8c844de8185e0b56
Nathan Castaldi <nathan@castaldi.dev>
2021-03-01 11:02:20 -0500

    added updated get and post routes with dynamic cart

--- routes/checkout.js
+++ routes/checkout.js
@@ -6,26 +6,33 @@ const stripe = require("stripe")(
 
 router
     .route("/")
+    .get(async (req, res) => {
+        const session = await stripe.checkout.sessions.retrieve(req.query.id, {
+            expand: ["line_items"],
+        });
+        res.json(session);
+    })
     .post(async (req, res) => {
+        const itemsInCheckout = req.body.cartItems.map((item) => {
+            return {
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: item.name,
+                        images: item.image,
+                    },
+                    unit_amount: item.price * 100,
+                },
+                quantity: 1,
+            };
+        });
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
-            line_items: [
-                {
-                    price_data: {
-                        currency: "usd",
-                        product_data: {
-                            name: "T-shirt",
-                        },
-                        unit_amount: 2000,
-                    },
-                    quantity: 1,
-                },
-            ],
+            line_items: itemsInCheckout,
             mode: "payment",
-            success_url: "https://www.google.com",
-            cancel_url: "https://www.bing.com",
+            success_url: "https://example.com/success.html",
+            cancel_url: "https://example.com/cancel.html",
         });
-
         res.json({ id: session.id });
     });
 
