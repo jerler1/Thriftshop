2be9a6ab6a19043afe39b79cabe0f5dd7edd629a
Nathan Castaldi <nathan@castaldi.dev>
2021-03-01 09:44:45 -0500

    removed stripe api call

--- server.js
+++ server.js
@@ -42,32 +42,6 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-const stripe = require("stripe")(
-  "sk_test_51IPhcIG7oxYUGKJCr6L1Htx1gIPshDLMp6gW1vkTl9dEmSJeVEPxqTJwU2c0xaoEklaTwFHEycrr5dUe36h4vaxg00vAFVLSjZ"
-);
-
-app.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "https://example.com/success.html",
-    cancel_url: "https://example.com/cancel.html",
-  });
-  res.json({ id: session.id });
-});
-
 app.use("/api", routes);
 
 // app.get("*", (req, res) => {
