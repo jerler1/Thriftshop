3f9e7cd43a4793f9afe3351e4510fe37d8189cbd
Bunks <busk_1@yahoo.com>
2021-03-01 13:31:32 -0500

    pruned package.json

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import API from "../../api/index";
 
 const Success = () => {
+  const [checkoutObject, setCheckoutObject] = useState({});
+
+  useEffect(() => {
+      
+  }, [])
   return <div></div>;
 };
 
