4376f7412b340c40ffdfb787d2b1e3900637bb88
Bradley Donahue <donahue.bradley@gmail.com>
2021-03-01 11:17:05 -0500

    Input working

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../api";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+    api
+      .doSearch(query)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="container mb-4 p-4">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field">
           <div className="control">
-            <input className="input" type="search" placeholder="Search..." />
+            <input className="input" type="search" placeholder="Search..." val={query} onChange={handleInputChange} />
           </div>
         </div>
       </form>
