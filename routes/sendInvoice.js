899ed5647d93d89417606835dddd4f6d7b785af9
Nathan Castaldi <nathan@castaldi.dev>
2021-03-01 17:24:57 -0500

    created initial nodemailer send route; not tested

---
+++ routes/sendInvoice.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+router
+    .route("/")
+    .post("/send", (req, res) => {
+        const invoiceBody = `
+            <p>Test Email</p>`;
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.office365.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: "nathan@castaldi.dev",
+                pass: "D!X20#4Z0w$BcL$fs1h6",
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"ThriftShopShop" <nathan@castaldi.dev>', // sender address
+            to: "nathan@castaldifamily.com", // list of receivers
+            subject: "Testing Nodemailer", // Subject line
+            text: "Is this thing on?", // plain text body
+            html: invoiceBody, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    })
