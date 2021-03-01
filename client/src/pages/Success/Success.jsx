227f5a96f94723904dd3f7d043d52b084802474b
Bunks <busk_1@yahoo.com>
2021-03-01 14:14:31 -0500

    added useeffect and state

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import API from "../../api/index";
 
 const Success = () => {
   const [checkoutObject, setCheckoutObject] = useState({});
+  const { id } = useParams();
 
   useEffect(() => {
-      
-  }, [])
-  return <div></div>;
+    API.getCheckOutObject(id).then((sessionObject) =>
+      setCheckoutObject(sessionObject).catch((err) => {
+        console.log(err);
+      })
+    );
+  }, []);
+  return <div>{checkoutObject}</div>;
 };
 
 export default Success;
