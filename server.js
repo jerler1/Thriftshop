73be74889b0b59c38c40be96afea3ea1f0a39524
Nathan Castaldi <nathan@castaldi.dev>
2021-03-01 11:03:42 -0500

    removed stripe api calls

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo").default;
 
 const routes = require("./routes");
-const checkout = require("./routes/checkout");
 
 const PORT = process.env.PORT || 3001;
 
@@ -39,49 +38,7 @@ mongoose
     console.log("Error connecting to MongoDB", err);
   });
 
-app.get("/api/config", (req, res) => {
-  res.json({
-    success: true,
-  });
-});
-
-const stripe = require("stripe")(
-  "sk_test_51IPhcIG7oxYUGKJCr6L1Htx1gIPshDLMp6gW1vkTl9dEmSJeVEPxqTJwU2c0xaoEklaTwFHEycrr5dUe36h4vaxg00vAFVLSjZ"
-);
-
-app.get("/checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.id, {
-    expand: ["line_items"],
-  });
-  res.json(session);
-});
-
-app.post("/create-checkout-session", async (req, res) => {
-  const itemsInCheckout = req.body.cartItems.map((item) => {
-    return {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.name,
-          images: item.image,
-        },
-        unit_amount: item.price * 100,
-      },
-      quantity: 1,
-    };
-  });
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: itemsInCheckout,
-    mode: "payment",
-    success_url: "https://example.com/success.html",
-    cancel_url: "https://example.com/cancel.html",
-  });
-  res.json({ id: session.id });
-});
-
 app.use("/api", routes);
-// app.use(checkout);
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client/build/index.html"));
