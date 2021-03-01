b81da5d2e95cc91cb3d0a9b5e7294e00166226d1
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-03-01 14:30:48 -0500

    cleaned up code by mapping out an array of category values

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
 
 const Sidebar = (props) => {
+
+    const categories = ["Art", "Clothing", "Electronics", "Entertainment", "Furniture", "Games/Toys", "Kitchenware", "Tools/Appliances"];
+
     return (
         <aside class="menu column is-2 thrift-sidebar">
           <p class="menu-label">Filter by Category!</p>
           <ul class="menu-list">
-            <li>
-              <a onClick={props.handleCatClick}>Art</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Clothing</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Electronics</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Entertainment</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Furniture</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Games/Toys</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Kitchenware</a>
-            </li>
-            <li>
-              <a onClick={props.handleCatClick}>Tools/Appliances</a>
-            </li>
+              {categories.map((category) => {
+                  return (<li><a onClick={props.handleCatClick}>{category}</a></li>)
+              })}
           </ul>
         </aside>
     );
