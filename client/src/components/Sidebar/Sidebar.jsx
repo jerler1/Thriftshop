0e73c8742cc290a49da3269085d30f0cb938a0c5
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-03-02 15:15:50 -0500

    re-adjusted the sidebar styling to work better on breakpoints

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -4,14 +4,14 @@ import "./Sidebar.css";
 const Sidebar = (props) => {
 
     const categories = ["Art", "Clothing", "Electronics", "Entertainment", "Furniture", "Games/Toys", "Kitchenware", "Tools/Appliances"];
-    const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(true);
     const setActive = () => {
         setIsActive(!isActive);
     };
 
     return (
         <aside className="menu column is-2 thrift-sidebar">
-          <p className="menu-label has-text-centered">Filter by Category! {isActive ? <i onClick={setActive} className="far fa-caret-square-down drop-caret"></i> : <i onClick={setActive} className="far fa-caret-square-up drop-caret"></i>}</p>
+          <p className="menu-label">Filter by Category! {isActive ? <i onClick={setActive} className="far fa-caret-square-down drop-caret"></i> : <i onClick={setActive} className="far fa-caret-square-up drop-caret"></i>}</p>
           <ul className={isActive ? "menu-list mobile-list" : "menu-list"}>
               <li>
                   <button className="button side-buttons" onClick={props.allClick}>All Items</button>
