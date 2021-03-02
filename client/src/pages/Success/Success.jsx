144b6137a92c258a67e0868c3fbe6c8b9a090aa6
Bunks <busk_1@yahoo.com>
2021-03-02 12:59:07 -0500

    added information for the post purchase page

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import API from "../../api/index";
+import "./Success.css";
+import photo from "../../images/happy.jpg";
 
 const Success = () => {
   const [checkoutObject, setCheckoutObject] = useState({});
@@ -15,11 +17,38 @@ const Success = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [id]);
   console.log(checkoutObject);
   return (
     <div>
-      <h1>Thank you for your purchase.</h1>
+      <div className="hero is-primary banner">
+        <div className="hero-body">
+          <h1 className="is-size-1 has-text-centered">
+            Thank you for your purchase.
+          </h1>
+          <h3 className="is-size-3 has-text-centered">Enjoy!</h3>
+        </div>
+      </div>
+      <div className="container">
+        <div className="columns">
+          <div className="column mt-6">
+            <ul className="postPurchaseMessages">
+              <li>Customer ID: {checkoutObject.customer}.</li>
+              <li>Status: {checkoutObject.payment_status}.</li>
+              <li>Subtotal: {checkoutObject.amount_subtotal / 100}.</li>
+              <li>
+                You will receive an email confirmation/invoice at{" "}
+                {checkoutObject?.customer_details?.email}.
+              </li>
+            </ul>
+          <Link className="button postButton" to="/">Return to Shopping</Link>
+            
+          </div>
+          <div className="column">
+              <img className="postPurchasePhoto" src={photo} alt="happy shopper picture"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
