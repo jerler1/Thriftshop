5ad64c6a4a3816d9c666b523404b4af4cb9ab144
Bunks <busk_1@yahoo.com>
2021-03-02 19:02:30 -0500

    added root route

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ mongoose
 app.use("/api", routes);
 
 if (process.env.PRODUCTION_URL) {
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
