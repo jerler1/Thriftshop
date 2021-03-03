b611cbc76268539df87ea51589a9945af726ef84
Bradley Donahue <donahue.bradley@gmail.com>
2021-03-02 21:32:35 -0500

    Update dashboard
    Includes an invoice table and displays for total orders and revenue.
    Moved add items button.

--- client/src/pages/Admin/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -53,26 +53,41 @@ const Dashboard = (props) => {
       });
   };
 
+  const totalRevenue = invoices.reduce((total, invoice) => {
+    return (
+      total +
+      invoice.purchasedItems.reduce((invoiceTotal, item) => {
+        return invoiceTotal + parseInt(item.itemTotal, 10);
+      }, 0)
+    );
+  }, 0);
+
+  const currencyFormatter = Intl.NumberFormat([], {
+    style: "currency",
+    currency: "usd",
+  });
+
+  const dateFormatter = Intl.DateTimeFormat([], {
+    dateStyle: "medium",
+  });
+
   return (
     <div className="container">
       <header className="mb-4">
         <h2 className="title">Welcome back, {auth.user.firstname}</h2>
-        <Link className="button is-primary is-outlined" to="/admin/addItem">
-          Add Items
-        </Link>
       </header>
       <section className="mb-4">
         <div
           className="mb-4"
           style={{
             display: "flex",
-            gap: "1rem"
+            gap: "1rem",
           }}
         >
           <div
             style={{
               borderRadius: 8,
-              width: "50%"
+              width: "50%",
             }}
             className="p-4 has-background-info-light"
           >
@@ -82,26 +97,45 @@ const Dashboard = (props) => {
           <div
             style={{
               borderRadius: 8,
-              width: "50%"
+              width: "50%",
             }}
             className="p-4 has-background-info-light"
           >
             <h2 className="title is-2">Total Revenue</h2>
             {/** compute the actual total.*/}
-            <p className="subtitle is-4 has-text-right">$1000.00</p>
+            <p className="subtitle is-4 has-text-right">{currencyFormatter.format(totalRevenue / 100)}</p>
           </div>
         </div>
         <h2 className="title is-4">Invoices</h2>
         {/** This should probably be a table. */}
-        <ol style={{
-          listStyle: 'none'
-        }}>
-          {invoices.map((invoice) => (
-            <li key={invoice._id}>{invoice.customerName} $29.99</li>
-          ))}
-        </ol>
+        <table className="table is-striped is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Email</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {invoices.map((invoice) => (
+              <tr key={invoice._id}>
+                <td>{dateFormatter.format(new Date(invoice.orderDate))}</td>
+                <td>{invoice.customerEmail}</td>
+                <td>{currencyFormatter.format(invoice.purchaseTotal)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
-      <Chart storeItems={storeItems} handleDelete={handleDelete} />
+      <div>
+        <div className="is-flex is-align-items-baseline" style={{ gap: "1rem" }}>
+          <h2 className="title is-4">Your Store's Items</h2>
+          <Link className="button is-primary is-outlined" to="/admin/addItem">
+            Add Items
+          </Link>
+        </div>
+        <Chart storeItems={storeItems} handleDelete={handleDelete} />
+      </div>
     </div>
   );
 };
